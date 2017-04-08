cadc9aa769d44ef95156673285a1fe871dc410cf
Andrew Montalto <aamontal@gmail.com>
2017-04-07 22:14:07 -0400

    lets play war!

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,66 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn shuffle-cards [cards]
+  (shuffle cards))
 
-(defn play-game [player1-cards player2-cards])
+(defn deal-cards [cards]
+  (let [shuffed (shuffle cards)]
+    [
+      (subvec shuffed, 0, (/ (count cards) 2))
+      (subvec shuffed, (/ (count cards) 2), (count cards))
+    ])
+)
+
+(defn rank [card]
+  (second card))
+
+(defn suit [card]
+  (first card))
+
+(defn rank-score [card]
+  (.indexOf ranks (rank card)))
+
+(defn suit-score [card]
+  (.indexOf suits (suit card)))
+
+(defn handle-rank-tie [player1-card player2-card]
+  (if (> (suit-score player1-card) (suit-score player2-card))
+    :player_1
+    :player_2
+  ))
+
+(defn play-round [player1-card player2-card]
+  (cond
+    (= (rank-score player1-card) (rank-score player2-card))
+      (handle-rank-tie player1-card player2-card)
+    (> (rank-score player1-card) (rank-score player2-card))
+      :player_1
+    :else
+      :player_2
+  ))
+
+(defn update-winner-deck [deck card1 card2]
+    (vec (concat (rest deck) [card1 card2])))
+
+(defn update-loser-deck [deck]
+  (rest deck))
+
+(defn play-game [player1-cards player2-cards round]
+  (println "round:" round)
+  (cond
+    (= player1-cards []) :player_2
+    (= player2-cards []) :player_1
+    :else
+      (let [
+        player1-card (first player1-cards)
+        player2-card (first player2-cards)
+        winner (play-round player1-card player2-card) ]
+        (println "1 card:" player1-card, "2 card: " player2-card " winner " winner)
+        (if (= winner :player_1)
+          (play-game (update-winner-deck player1-cards player2-card player1-card)
+                     (update-loser-deck player2-cards)
+                     (+ round 1))
+          (play-game (update-loser-deck player1-cards)
+                     (update-winner-deck player2-cards player1-card player2-card)
+                     (+ round 1)) ))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -6,14 +6,35 @@
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= :player_1 (play-round [:heart 9] [:club 8])))
+    (is (= :player_2 (play-round [:heart 8] [:club 9])))
+  )
+  (testing "queens are higher rank than jacks"
+    (is (= :player_1 (play-round [:heart :queen] [:heart :jack])))
+    (is (= :player_2 (play-round [:heart :jack] [:club :queen])))
+  )
+  (testing "kings are higher rank than queens"
+    (is (= :player_1 (play-round [:heart :king] [:heart :queen])))
+    (is (= :player_2 (play-round [:heart :queen] [:club :king])))
+  )
+  (testing "aces are higher rank than kings"
+    (is (= :player_1 (play-round [:heart :ace] [:heart :king])))
+    (is (= :player_2 (play-round [:heart :king] [:heart :ace])))
+  )
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= :player_1 (play-round [:club :ace] [:spade :ace])))
+    (is (= :player_2 (play-round [:spade :ace] [:club :ace])))
+  )
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= :player_1 (play-round [:diamond :ace] [:club :ace])))
+    (is (= :player_2 (play-round [:club :ace] [:diamond :ace])))
+  )
+  (testing "if the ranks are equal, hearts beat diamonds")
+    (is (= :player_1 (play-round [:heart :ace] [:diamond :ace])))
+    (is (= :player_2 (play-round [:diamond :ace] [:heart :ace])))
+  )
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards")
+    (is (= :player_1 (play-game [[:heart :ace]] [[:diamond :ace]])))
+  )
